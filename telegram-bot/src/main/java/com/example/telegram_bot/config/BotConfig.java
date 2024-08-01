c096d8a6a73602596a1541b1f222d7a0d8c0d238
ShuMAhd <shuman@zykaika.ru>
2024-08-01 19:48:33 +0800

    Создан BotConfig

--- telegram-bot/src/main/java/com/example/telegram_bot/config/BotConfig.java
+++ telegram-bot/src/main/java/com/example/telegram_bot/config/BotConfig.java
@@ -1,5 +1,29 @@
 package com.example.telegram_bot.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class BotConfig {
 
+  @Value("${telegram.bot.username}")
+  private String botUsername;
+
+  @Value("${telegram.bot.token}")
+  private String botToken;
+
+  @Value("${telegram.lawyers.chatId}")
+  private String lawyersChatId;
+
+  public String getBotUsername() {
+    return botUsername;
+  }
+
+  public String getBotToken() {
+    return botToken;
+  }
+
+  public String getLawyersChatId() {
+    return lawyersChatId;
+  }
 }
